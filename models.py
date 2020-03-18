98247e1ec4f18478642030209518b93b1aedb952
Diljot Singh <45079666+DiljotSingh@users.noreply.github.com>
2020-03-18 00:52:04 -0700

    Updated models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 from google.appengine.ext import ndb
 
 class Meme(ndb.Model):
-  first_line = ndb.StringProperty(required=True)
+  first_line = ndb.StringProperty(required=False)
   second_line = ndb.StringProperty(required=False)
   pic_type = ndb.StringProperty(required=False)
 
